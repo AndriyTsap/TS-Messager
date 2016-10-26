a7530124ef1346a4f7e2ee12cc5cfbffd5c63e99
Andrii <tsap.andriy@gmail.com>
2016-10-26 13:43:05 +0300

    change paths to files in appsettings.json and now AppSettings.cs append an current directory to json files. Alsow registrate some services in ServiceLocator
    Signed-off-by: Andrii <tsap.andriy@gmail.com>

--- aspnet5-angular2-typescript/src/ConsoleApp1/AppSettings.cs
+++ aspnet5-angular2-typescript/src/ConsoleApp1/AppSettings.cs
@@ -7,11 +7,13 @@ namespace ConsoleApp1
     public class AppSettings
     {
         private IConfigurationRoot Configuration { get; set; }
+        private readonly string _currentDirectory;
 
         private AppSettings()
         {
+            _currentDirectory = Directory.GetCurrentDirectory();
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
+                .SetBasePath(_currentDirectory)
                 .AddJsonFile(@"appsettings.json");
 
             string path = Directory.GetCurrentDirectory();
@@ -33,22 +35,22 @@ namespace ConsoleApp1
 
         public string UsersFilePath
         {
-            get { return "Data:XmlFiles:UserFilePath"; }
+            get { return _currentDirectory+Configuration["Data:XmlFiles:UserFilePath"]; }
         }
 
         public string MessagesFilePath
         {
-            get { return Configuration["Data:XmlFiles:MessageFilePath"]; }
+            get { return _currentDirectory + Configuration["Data:XmlFiles:MessageFilePath"]; }
         }
 
         public string GroupsFilePath
         {
-            get { return Configuration["Data:XmlFiles:GroupFilePath"]; }
+            get { return _currentDirectory + Configuration["Data:XmlFiles:GroupFilePath"]; }
         }
 
         public string UserGroupFilePath
         {
-            get { return Configuration["Data:XmlFiles:UserGroupFilePath"]; }
+            get { return _currentDirectory + Configuration["Data:XmlFiles:UserGroupFilePath"]; }
         }
 
         public string ConnectionString
