4934a033f919f7a7cfce58913611f2c76571126d
Andrii <tsap.andriy@gmail.com>
2016-10-29 19:57:51 +0300

    fixed bug, when logger can`t take null exception
    Signed-off-by: Andrii <tsap.andriy@gmail.com>

--- aspnet5-angular2-typescript/src/ConsoleApp1/Services/Logger.cs
+++ aspnet5-angular2-typescript/src/ConsoleApp1/Services/Logger.cs
@@ -10,18 +10,20 @@ namespace ConsoleApp1.Services
 {
     public class Logger: ILogger
     {
+        private ILoggingRepository _loggingRepository;
+
+        public Logger(ILoggingRepository loggingRepository)
+        {
+            _loggingRepository = loggingRepository;
+        }
 
         public void Log(LogEntry entry)
         {
-
-            var optionsBuilder = new DbContextOptionsBuilder<PhotoGalleryContext>();
-            optionsBuilder.UseSqlServer(AppSettings.Instance.ConnectionString);
-            var context = new PhotoGalleryContext(optionsBuilder.Options);
-            var logginingRepository = new LoggingRepository(context);
-            
-            logginingRepository.Add(new Error() {Severity = entry.Severity.ToString(), DateCreated = DateTime.Now, Message = entry.Message, StackTrace = entry.Exception.StackTrace});
-            logginingRepository.Commit();
-
+            if (entry.Exception != null)
+                _loggingRepository.Add(new Error {Severity = entry.Severity.ToString(), DateCreated = DateTime.Now, Message = entry.Message, StackTrace = entry.Exception.StackTrace});
+            else
+                _loggingRepository.Add(new Error { Severity = entry.Severity.ToString(), DateCreated = DateTime.Now, Message = entry.Message, StackTrace = "" });
+            _loggingRepository.Commit();
         }
     }
 }
