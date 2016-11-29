a4bc2927943ac4a09d455a4645194e54fa745073
Andrii <tsap.andriy@gmail.com>
2016-11-29 15:06:24 +0200

    testing getFriends method
    Signed-off-by: Andrii <tsap.andriy@gmail.com>

--- aspnet5-angular2-typescript/src/APITests/FriendsSearcherTests.cs
+++ aspnet5-angular2-typescript/src/APITests/FriendsSearcherTests.cs
@@ -1,8 +1,13 @@
 using System;
+using Microsoft.EntityFrameworkCore;
 using NUnit;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using Moq;
+using PhotoGallery.Infrastructure;
+using PhotoGallery.Infrastructure.Repositories;
+using PhotoGallery.Infrastructure.Repositories.Abstract;
+using PhotoGallery.Infrastructure.Services;
 
 namespace APITests
 {
@@ -12,7 +17,17 @@ namespace APITests
         [Test]
         public void ShouldBeOk_IfIt_CanCheckUsers_OnFrienship()
         {
+            var data = new MockData();
 
+            var userRepo = ServiceLocator.Instance.Resolve<IUserRepository>();
+            var chatRepo = ServiceLocator.Instance.Resolve<IChatRepository>();
+            var chatUserRepo = ServiceLocator.Instance.Resolve<IChatUserRepository>();
+
+            var friendsSearcher = new FriendsSearcher(userRepo, chatRepo, chatUserRepo);
+
+            var friends = friendsSearcher.GetFriends(1);
+
+            Assert.That(true);
         }
     }
 }
