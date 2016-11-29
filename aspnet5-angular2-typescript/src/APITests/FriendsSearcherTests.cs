d9b39280c6559935babba8bbdb4b7941747f70f7
Andrii <tsap.andriy@gmail.com>
2016-11-29 09:50:46 +0200

    preparing for testing FriendsSearcher class
    Signed-off-by: Andrii <tsap.andriy@gmail.com>

---
+++ aspnet5-angular2-typescript/src/APITests/FriendsSearcherTests.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Moq;
+
+namespace APITests
+{
+    [TestFixture]
+    public class FriendsSearcherTests
+    {
+        [Test]
+        public void ShouldBeOk_IfIt_CanCheckUsers_OnFrienship()
+        {
+
+        }
+    }
+}
